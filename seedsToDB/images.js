f701b4602df89ae580f728523aec1f403bcb010e
Renato <renato.f.fabri@gmail.com>
2022-11-03 16:28:03 -0300

    First commit

---
+++ seedsToDB/images.js
@@ -0,0 +1,42 @@
+module.exports.imagesArray = [
+  {
+    url: "https://res.cloudinary.com/r73fgem9f/image/upload/v1667264520/YelpCamp/tegan-mierle-fDostElVhN8-unsplash_eaaaoj.jpg",
+    filename: "YelpCamp/tegan-mierle-fDostElVhN8-unsplash_eaaaoj",
+  },
+  {
+    url: "https://res.cloudinary.com/r73fgem9f/image/upload/v1667166791/YelpCamp/reuben-kim-GTdi6j8Qnh8-unsplash_uc39km.jpg",
+    filename: "YelpCamp/reuben-kim-GTdi6j8Qnh8-unsplash_uc39km",
+  },
+  {
+    url: "https://res.cloudinary.com/r73fgem9f/image/upload/v1667166736/YelpCamp/tommy-lisbin-2DH-qMX6M4E-unsplash_lu48ry.jpg",
+    filename: "YelpCamp/tommy-lisbin-2DH-qMX6M4E-unsplash_lu48ry",
+  },
+  {
+    url: "https://res.cloudinary.com/r73fgem9f/image/upload/v1667166736/YelpCamp/stephen-wheeler-hBh9JbyeCtg-unsplash_w6tpzd.jpg",
+    filename: "YelpCamp/stephen-wheeler-hBh9JbyeCtg-unsplash_w6tpzd",
+  },
+  {
+    url: "https://res.cloudinary.com/r73fgem9f/image/upload/v1667166736/YelpCamp/laura-pluth-RMicIhNOOIg-unsplash_etlw8c.jpg",
+    filename: "YelpCamp/laura-pluth-RMicIhNOOIg-unsplash_etlw8c",
+  },
+  {
+    url: "https://res.cloudinary.com/r73fgem9f/image/upload/v1667166736/YelpCamp/ryan-stone-assZIxUnw8I-unsplash_l2p2bl.jpg",
+    filename: "YelpCamp/ryan-stone-assZIxUnw8I-unsplash_l2p2bl",
+  },
+  {
+    url: "https://res.cloudinary.com/r73fgem9f/image/upload/v1667166736/YelpCamp/zach-betten-K9olx8OF36A-unsplash_rlcjut.jpg",
+    filename: "YelpCamp/zach-betten-K9olx8OF36A-unsplash_rlcjut",
+  },
+  {
+    url: "https://res.cloudinary.com/r73fgem9f/image/upload/v1667166736/YelpCamp/johannes-plenio-ub0HbYE5c0s-unsplash_mfpd4o.jpg",
+    filename: "YelpCamp/johannes-plenio-ub0HbYE5c0s-unsplash_mfpd4o",
+  },
+  {
+    url: "https://res.cloudinary.com/r73fgem9f/image/upload/v1648582238/YelpCamp/bstvkhx2wasksb1b45h6.jpg",
+    filename: "YelpCamp/bstvkhx2wasksb1b45h6",
+  },
+  {
+    url: "https://res.cloudinary.com/r73fgem9f/image/upload/v1648582237/YelpCamp/hxck803ofd1l5mxkon2q.jpg",
+    filename: "YelpCamp/hxck803ofd1l5mxkon2q",
+  },
+];
---
+++ seedsToDB/index.js
@@ -0,0 +1,76 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const Campground = require("../models/campground");
+const { places, descriptors } = require("./seedHelpers");
+const { imagesArray } = require("./images");
+const { description } = require("./description");
+
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
+
+mongoose.connect(dbUrl);
+
+const db = mongoose.connection;
+
+//THIS IS THE AMOUNT OF CAMPGROUNDS TO SEED.
+const SEED_AMOUNT = 200;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database Connected");
+});
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+//authors id's gotten from the database.
+const authors = [
+  "635c563748aa720b9f69fd84",
+  "635c566548aa720b9f69fd8b",
+  "635c567d48aa720b9f69fd91",
+  "635c569748aa720b9f69fd98",
+];
+
+const seedDB = async () => {
+  //BE CAREFUL, THIS WILL CLEAR THE ENTIRE DATABASE BEFORE FILLING IT AGAIN.
+  await Campground.deleteMany({});
+  for (let i = 0; i < SEED_AMOUNT; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
+    const imagesToAdd = () => {
+      let indexes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+      let randomValue = 0;
+      const randomArray = [];
+
+      for (let i = 0; i <= 2; i++) {
+        randomValue = Math.floor(Math.random() * indexes.length);
+        randomArray.push(imagesArray[indexes[randomValue]]);
+        indexes.splice(randomValue, 1);
+      }
+      return randomArray;
+    };
+    const camp = new Campground({
+      author: sample(authors),
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      description: sample(description),
+      price,
+      geometry: {
+        type: "Point",
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude,
+        ],
+      },
+      images: imagesToAdd(),
+    });
+    await camp.save();
+  }
+  console.log("All is Done!");
+};
+
+seedDB().then(() => {
+  mongoose.connection.close();
+});
